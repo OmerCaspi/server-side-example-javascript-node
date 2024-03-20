7be589d48281a753cf3dd3dad18146b8c11c2ce1
OmerCaspi <43809554+OmerCaspi@users.noreply.github.com>
2024-03-20 09:59:47 +0200

    Update router.ts

--- src/router.ts
+++ src/router.ts
@@ -14,19 +14,19 @@ router.use((req, res, next) => {
 
   // Check request body to see if message object has type "status-update" and if so, log the status property
   if (req.body.message && req.body.message.type === 'status-update') {
-    console.log(`Status: ${req.body.message.status}\n`);
+    console.log(`Status: ${req.body.message.status}`);
   }
 
   // If message type is "end-of-call-report", log endedReason property
   if (req.body.message && req.body.message.type === 'end-of-call-report') {
-    console.log(`Ended Reason: ${req.body.message.endedReason}\n`);
+    console.log(`Ended Reason: ${req.body.message.endedReason}`);
 
     // Go over messages array, and if role is not "system" the log the role and the message properties
     if (req.body.message.messages) {
       req.body.message.messages.forEach((message: any) => {
         if (message.role !== 'system') {
           console.log(`Role: ${message.role}`);
-          console.log(`Message: ${message.message}\n`);
+          console.log(`Message: ${message.message}`);
         }
       });
     }
